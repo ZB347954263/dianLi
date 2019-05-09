cf2e1f7f470eee5fe041c306901d3b2814f4d8ef
张博 <347954263@qq.com>
2019-05-09 23:54:00 +0800

    产品中心

--- src/pages/Product/components/Products/Products.jsx
+++ src/pages/Product/components/Products/Products.jsx
@@ -34,7 +34,14 @@ export default class Products extends Component {
   renderProductItemDetail = (item, index) => {
     if(index == this.state.indexCurrent) {
         return (
-        <div>{item.content}</div>
+        <div>{item.content}
+             {item.picList.map((pic, picIndex) => {
+                return (
+                    <img src={pic} alt={item.code} style={styles.pic}/>
+                )
+              })
+             }
+        </div>
         );
     }
   };
@@ -61,34 +68,163 @@ export default class Products extends Component {
 
 const dataSource = [
   {
-    title: '环网柜',
+    code: '1-KYN28A-12',
+    title: '铠装移开式金属封闭开关设备',
     current:true,
-    content:'环网柜',
+    content:'铠装移开式金属封闭开关设备'
+    ,picList:[
+        require('./img/4.jpg'),
+        require('./img/5.jpg'),
+        require('./img/6.jpg')
+    ]
   },
   {
-    title: '充气柜',
+    code: '2-KYN28-12',
+    title: '铠装中置式金属封闭开关设备',
     current:false,
-    content:'充气柜',
-  },
-   {
-    title: '固体柜',
-    current:false,
-    content:'固体柜',
-  },
-   {
-    title: '中置柜',
-    current:false,
-    content:'中置柜',
+    content:'铠装中置式金属封闭开关设备'
+    ,picList:[
+        require('./img/4.jpg'),
+        require('./img/5.jpg'),
+        require('./img/6.jpg')
+    ]
   },
   {
-    title: '箱变分支箱',
+    code: '3-XGN66-12',
+    title: '固定式封闭开关设备',
     current:false,
-    content:'箱变分支箱',
+    content:'固体柜'
+    ,picList:[
+        require('./img/4.jpg'),
+        require('./img/5.jpg'),
+        require('./img/6.jpg')
+    ]
   },
+   {
+    code: '4-KYN61-40.5',
+    title: '铠装移开式交流金属封闭开关设备',
+    current:false,
+    content:'铠装移开式交流金属封闭开关设备'
+    ,picList:[
+        require('./img/4.jpg'),
+        require('./img/5.jpg'),
+        require('./img/6.jpg')
+    ]
+  },
+  {
+    code: '5-GGD',
+    title: '交流电柜',
+    current:false,
+    content:'交流电柜'
+    ,picList:[
+        require('./img/4.jpg'),
+        require('./img/5.jpg'),
+        require('./img/6.jpg')
+    ]
+  },
+  {
+    code: '6-GCS',
+    title: '低压开关柜',
+    current:false,
+    content:'低压开关柜'
+    ,picList:[
+        require('./img/4.jpg'),
+        require('./img/5.jpg'),
+        require('./img/6.jpg')
+    ]
+  },
+  {
+    code: '7-GCK',
+    title: '低压开关柜',
+    current:false,
+    content:'低压开关柜'
+    ,picList:[
+        require('./img/4.jpg'),
+        require('./img/5.jpg'),
+        require('./img/6.jpg')
+    ]
+  },
+  {
+    code: '8-GGJ',
+    title: '低压电容柜',
+    current:false,
+    content:'低压电容柜'
+    ,picList:[
+        require('./img/4.jpg'),
+        require('./img/5.jpg'),
+        require('./img/6.jpg')
+    ]
+  }
+  ,{
+    code: '9-MNS',
+    title: '低压开关柜',
+    current:false,
+    content:'低压开关柜'
+    ,picList:[
+        require('./img/4.jpg'),
+        require('./img/5.jpg'),
+        require('./img/6.jpg')
+    ]
+  }
+  ,{
+    code: '10-欧式箱变',
+    title: '变电箱箱变',
+    current:false,
+    content:'变电箱箱变'
+    ,picList:[
+        require('./img/4.jpg'),
+        require('./img/5.jpg'),
+        require('./img/6.jpg')
+    ]
+  }
+  ,{
+    code: '11-美式箱变',
+    title: '变电箱箱变',
+    current:false,
+    content:'变电箱箱变'
+    ,picList:[
+        require('./img/4.jpg'),
+        require('./img/5.jpg'),
+        require('./img/6.jpg')
+    ]
+  }
+  ,{
+    code: '12-JP柜',
+    title: '综合配电箱',
+    current:false,
+    content:'综合配电箱'
+    ,picList:[
+        require('./img/4.jpg'),
+        require('./img/5.jpg'),
+        require('./img/6.jpg')
+    ]
+  }
+  ,{
+    code: '13-XLF-21',
+    title: '动力配电柜',
+    current:false,
+    content:'动力配电柜'
+    ,picList:[
+        require('./img/4.jpg'),
+        require('./img/5.jpg'),
+        require('./img/6.jpg')
+    ]
+  }
+  ,{
+    code: '14-GZDW-06',
+    title: '直流屏',
+    current:false,
+    content:'直流屏'
+    ,picList:[
+        require('./img/4.jpg'),
+        require('./img/5.jpg'),
+        require('./img/6.jpg')
+    ]
+  }
 ]
 const styles = {
   mainContent:{
-    width: '1200px',
+    width: '1300px',
     height: 'auto',
     background:'#f0f8fb',
     // background: '0% 0% / cover rgb(255, 255, 255)',
@@ -118,4 +254,8 @@ const styles = {
     flexDirection: 'row',
     justifyContent: 'flex-end',
   },
+  pic:{
+      width:'100%',
+      height:'auto',
+  }
 };
